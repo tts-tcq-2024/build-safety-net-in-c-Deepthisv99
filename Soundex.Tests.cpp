9e0101291bed625c69aed36159feef87a9d31b90
Deepthisv99 <deepthivenki1@gmail.com>
2024-07-04 10:43:40 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -4,6 +4,6 @@
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
-  GenerateSoundex("AX", soundex);
+  GenerateSoundex("AX");
  // ASSERT_EQ(soundex,"A200");
 }
