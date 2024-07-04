f2c082cc1fa5e56ac12e58bff14f820b614ca761
Deepthisv99 <deepthivenki1@gmail.com>
2024-07-04 16:43:17 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -18,5 +18,27 @@ char getSoundexCode(char c) {
     }
 }
 
+
+void generateSoundex(const char *name, char *soundex) {
+    int len = strlen(name);
+    soundex[0] = toupper(name[0]);
+    int sIndex = 1;
+
+    for (int i = 1; i < len && sIndex < 4; i++) {
+        char code = getSoundexCode(name[i]);
+        if (code != '0' && code != soundex[sIndex - 1]) {
+            soundex[sIndex++] = code;
+        }
+    }
+
+    while (sIndex < 4) {
+        soundex[sIndex++] = '0';
+    }
+
+    soundex[4] = '\0';
+}
+
+#endif // SOUNDEX_H
+
 #endif
 
